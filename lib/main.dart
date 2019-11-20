a9c9bf1dc9b6f569695362c81b534c076f150851
MakamuEvans <makamuevans@gmail.com>
2019-11-20 17:18:21 +0300

    added interactivity

--- lib/main.dart
+++ lib/main.dart
@@ -8,24 +8,94 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Makamu Evans",
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Elm"),
-        ),
-        body: Center(
-          child: RandomWords(),
-        ),
+      theme: ThemeData(
+        primaryColor: Colors.white
       ),
+      home: RandomWords()
     );
   }
   
 }
 
 class RandomWordState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final Set<WordPair> _saved = Set<WordPair>();
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    return Text(wordPair.asPascalCase);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Startup Name Generator"),
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.list), onPressed: _pushSaved),
+        ],
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+
+  void _pushSaved(){
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+          builder: (BuildContext context){
+            final Iterable<ListTile> tiles = _saved.map(
+                (WordPair pair){
+                  return ListTile(
+                    title: Text(
+                        pair.asPascalCase,
+                        style: _biggerFont
+                    )
+                  );
+                }
+            );
+            final List<Widget> divided = ListTile
+            .divideTiles(tiles: tiles, context: context)
+            .toList();
+
+            return Scaffold(
+              appBar: AppBar(
+                title: Text("Saved Suggestions"),
+              ),
+              body: ListView(children: divided),
+            );
+          },
+      )
+    );
+  }
+
+  Widget _buildSuggestions(){
+    return ListView.builder(itemBuilder:  (context, i){
+      if (i.isOdd) return Divider();
+      final index = i ~/ 2;
+      if (index >= _suggestions.length){
+        _suggestions.addAll(generateWordPairs().take(10));
+      }
+      return _buildRow(_suggestions[index]);
+    });
+  }
+
+  Widget _buildRow(WordPair pair){
+    final bool alreadySaved = _saved.contains(pair);
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {
+        setState(() {
+          if (alreadySaved){
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
+    );
   }
 
 }
